65abe9bb3d5dfd0eb6ac4b58e2d7d9827ecd6376
Jean Rauwers <jeanrauwers@gmail.com>
2020-02-21 19:20:28 +0000

    Gorilla mux and contact

--- main.go
+++ main.go
@@ -7,14 +7,19 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func handleReturn(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Helloss` from go server</h1>")
+func contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Get in Touch</h1>")
+	fmt.Fprint(w, "<h3>@jeanrauwers</h3>")
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello to Go Web Server</h1>")
 }
 
 func main() {
-
 	r := mux.NewRouter()
-	r.HandleFunc("/", handleReturn)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+
 	http.ListenAndServe(":3000", r)
 }
