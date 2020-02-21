114f7017ac2456d587a39101542728098cc14a64
Jean Rauwers <jeanrauwers@gmail.com>
2020-02-21 19:38:31 +0000

    Add 404 page

--- main.go
+++ main.go
@@ -16,10 +16,15 @@ func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello to Go Web Server</h1>")
 }
 
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>404!!!! Page not Found</h1>")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.NotFoundHandler = r.NewRoute().HandlerFunc(notFoundHandler).GetHandler()
 
 	http.ListenAndServe(":3000", r)
 }
