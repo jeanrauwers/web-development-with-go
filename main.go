6cf93afcfffdc18be7c8b40ecb7827047ba68fea
Jean Rauwers <jeanrauwers@gmail.com>
2020-02-21 19:14:30 +0000

    Add gorila mux

--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func handleReturn(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Someone visited our page")
-	fmt.Fprint(w, "<h1>Hello` from go server</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Helloss` from go server</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", handleReturn)
-	http.ListenAndServe(":3000", nil)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", handleReturn)
+	http.ListenAndServe(":3000", r)
 }
