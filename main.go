1b7fe2ef2c1b11aab9b14a328e63c5d55176a901
Jean Rauwers <jeanrauwers@gmail.com>
2020-04-18 13:00:33 +0100

    Add 404 handler

--- main.go
+++ main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
-
 	"github.com/julienschmidt/httprouter"
 )
 
 func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "<h1>Get in Touch</h1>")
+	fmt.Fprint(w, "<h1>Get in Touch...</h1>")
 	fmt.Fprint(w, "<h3>@jeanrauwers</h3>")
 }
 
@@ -16,8 +15,9 @@ func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "<h1>Hello to Go Web Server</h1>")
 }
 
-func notFoundHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "<h1>404!!!! Page not Found</h1>")
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>404!!!! Page not Found JR</h1>")
 }
 
 func main() {
@@ -25,7 +25,8 @@ func main() {
 
 	router.GET("/", home)	
 	router.GET("/contact", contact)	
+	router.NotFound =  http.HandlerFunc(notFoundHandler)
 
 
-	http.ListenAndServe(":3333", router)
+	http.ListenAndServe("localhost:3333", router)
 }
