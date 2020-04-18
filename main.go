5bc7f770ead1636d183a14bd5e8270e9cf2833a1
Jean Rauwers <jeanrauwers@gmail.com>
2020-04-18 12:42:28 +0100

    Add httprouter to project

--- main.go
+++ main.go
@@ -4,27 +4,28 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 )
 
-func contact(w http.ResponseWriter, r *http.Request) {
+func Contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "<h1>Get in Touch</h1>")
 	fmt.Fprint(w, "<h3>@jeanrauwers</h3>")
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
+func Home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "<h1>Hello to Go Web Server</h1>")
 }
 
-func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+func notFoundHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "<h1>404!!!! Page not Found</h1>")
 }
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.NotFoundHandler = r.NewRoute().HandlerFunc(notFoundHandler).GetHandler()
+	router := httprouter.New()
 
-	http.ListenAndServe(":3000", r)
+	router.GET("/", Home)	
+	router.GET("/contact", Contact)	
+
+
+	http.ListenAndServe(":3333", router)
 }
