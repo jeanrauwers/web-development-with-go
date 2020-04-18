274a56ffd6e42ddf0e5872da1bf9244812867e49
Jean Rauwers <jeanrauwers@gmail.com>
2020-04-18 13:17:33 +0100

    Reverting to gorilla mux

--- main.go
+++ main.go
@@ -3,30 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"github.com/julienschmidt/httprouter"
+
+	"github.com/gorilla/mux"
 )
 
-func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "<h1>Get in Touch...</h1>")
+func contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Get in Touch</h1>")
 	fmt.Fprint(w, "<h3>@jeanrauwers</h3>")
 }
 
-func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello to Go Web Server</h1>")
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h1>404!!!! Page not Found JR</h1>")
+	fmt.Fprint(w, "<h1>404!!!! Page not Found</h1>")
 }
 
 func main() {
-	router := httprouter.New()
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.NotFoundHandler = r.NewRoute().HandlerFunc(notFoundHandler).GetHandler()
 
-	router.GET("/", home)	
-	router.GET("/contact", contact)	
-	router.NotFound =  http.HandlerFunc(notFoundHandler)
-
-
-	http.ListenAndServe("localhost:3333", router)
+	http.ListenAndServe("localhost:3000", r)
 }
